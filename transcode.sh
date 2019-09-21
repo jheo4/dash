2aaa44fcadd72590085f984e231f261717d49199
Jin Heo <993jin@gmail.com>
2019-09-22 01:06:12 +0900

    script

--- transcode.sh
+++ transcode.sh
@@ -2,23 +2,23 @@ MYDIR=$(pwd)
 INPUTDIR=$(pwd)"/Inputs"
 OUTPUTDIR=$(pwd)"/Outputs"
 
-source $(pwd)"/audio.sh"
-source $(pwd)"/do_transcode.sh"
-source $(pwd)"/generate_dash_files.sh"
+. $(pwd)"/audio.sh"
+. $(pwd)"/do_transcode.sh"
+. $(pwd)"/generate_dash_files.sh"
 
 if [ -z "$MYDIR/bin/ffmpeg" ]; then
   echo "No ffmpeg..."
   exit 1
 fi
 
-if [ -z "$(which MP4Box)" ]; then
-  echo "No MP4Box..."
-  exit 1
-fi
+#if [ -z "$(which MP4Box)" ]; then
+#  echo "No MP4Box..."
+#  exit 1
+#fi
 
 MP4_FILES=$(find ./Inputs/ -type f \( -name "*.mp4" \))
-for f in $MP4_FILES
-do
+
+for f in $MP4_FILES; do
   input_file=$(basename "$f")
   file_name="${input_file%.*}" # FN without extension: "${FNext%.*}"
                                # extension from FN.ext "${FNext##*.}"
@@ -39,9 +39,6 @@ do
   # do_transcode INPUT OUTPUT CODEC RESOLUTION BITRATE CONTAINER
   resolutions=("480" "720" "1080" "4k")
   codecs=("libx264" "mpeg4" "libvpx-vp9")
-
-  #resolutions=("480")
-  #codecs=("libvpx-vp9")
 
   for cod in ${codecs[*]}; do
     if [ ! -d "$OUTPUTDIR/${file_name}/$cod" ]; then
@@ -66,13 +63,12 @@ do
       elif [ $res = "4k" ]; then
         br="8M"
       fi
-      #do_transcode "$INPUTDIR/${input_file}" "$output" "$cod" "$res" "$br"\
-      #  "$container"
+      do_transcode "$INPUTDIR/${input_file}" "$output" "$cod" "$res" "$br"\
+        "$container"
     done
 
     # generate_dash_files OUTPUTDIR FN CODEC RESOLUTIONS CONTAINER
     generate_dash_files "$OUTPUTDIR" "${file_name}" "$cod" "$resolutions"\
       "$container"
   done
-
 done
